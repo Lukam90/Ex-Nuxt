0a6b6b44aa72ed0abed7b6ff99a5dc199169112c
Lucien HAMM <lucien.hamm@gmail.com>
2024-03-20 21:56:02 +0100

    MAJ Ex Nuxt > Fetch Data

--- nuxt-course/pages/products/[id].vue
+++ nuxt-course/pages/products/[id].vue
@@ -1,13 +1,17 @@
 <template>
     <div>
-        <p>Product details for {{ id }}</p>
-
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui sapiente soluta quod natus consectetur aliquid?</p>
+        <p>{{ product.title }}</p>
+        <p>{{ product.price }}</p>
+        <p>{{ product.id }}</p>
     </div>
 </template>
 
 <script setup>
 const { id } = useRoute().params
+
+const url = "https://fakestoreapi.com/products/" + id
+
+const { data: product } = await useFetch(url, { key: id })
 
 definePageMeta({
   layout: "products"
