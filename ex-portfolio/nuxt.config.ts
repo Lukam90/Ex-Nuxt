60c5b74a67823fff167c38487d9af605cecbdd66
Lucien HAMM <lucien.hamm@gmail.com>
2024-03-21 13:13:18 +0100

    MAJ Ex Portfolio > Blog Design

--- ex-portfolio/nuxt.config.ts
+++ ex-portfolio/nuxt.config.ts
@@ -1,8 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  //devtools: { enabled: true },
   modules: [
     '@nuxtjs/tailwindcss',
+    '@nuxt/content',
     'nuxt-icon'
-  ]
+  ],
+  content: {
+    // https://content.nuxjs.org/api/configuration
+    highlight: {
+      theme: "nord",
+      preload: ["ts", "js", "css", "java", "json", "bash", "vue"]
+    }
+  }
 })
